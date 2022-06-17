babb77f3368d89e838bc6289d5f04f1df3b56948
nidroy <62301362+nidroy@users.noreply.github.com>
2022-06-18 00:34:30 +0300

    animation refactoring
    рефакторинг анимаций

--- Unknown Worlds of Knowledge/Assets/Scripts/StartMenu.cs
+++ Unknown Worlds of Knowledge/Assets/Scripts/StartMenu.cs
@@ -1,8 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class StartMenu : MonoBehaviour
 {
-    
+    public Animator anim;
+
+    private bool isStart;
+
+    public void StartGame()
+    {
+        anim.SetBool("isHideInterface", true);
+    }
+
+    private void Start()
+    {
+        isStart = true;
+    }
+    private void Update()
+    {
+        if(isStart && Input.anyKey)
+        {
+            anim.SetBool("isShowInterface", true);
+            isStart = false;
+        }
+    }
 }
